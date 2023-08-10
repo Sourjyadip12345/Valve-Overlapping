2255d022167b2e85e4d8348310506c73af8c59fb
amnnsharma <98727525+amnnsharma@users.noreply.github.com>
2023-08-10 10:30:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -142,25 +142,7 @@ def analysis():
 
 
             font_properties = FontProperties(family='serif', size=15, weight='normal', style='italic')
-            '''
-            sl_no=[i+1 for i in range(len(arrow_labels))]
-            first=1
-            first_overlap=0
-            for x_val,label,_ in sorted(zip(schedule, arrow_labels,sl_no)):
-                    
-                if x_val<=first_overlap:
-                    first_overlap=x_val+len(arrow_labels)*int(len(sum_timings)/50)
-                    ax.annotate(str(label), xy=(x_val, 0), xytext=(x_val, -1.1*first), fontproperties=font_properties)
-                    first+=1
-                    if first>=3:
-                        first=1
-
-                else:
-                    first=1
-                    first_overlap=x_val+len(arrow_labels)*int(len(sum_timings)/50)
-                    ax.annotate(label, xy=(x_val, 0), xytext=(x_val, -1.1*first), fontproperties=font_properties)
-            '''
-
+            
                 
             for value in schedule:
                 ax.arrow(value,-0.2, 0, 0.2, head_width=2.5, head_length=0.1, fc='black', ec='black')
@@ -549,7 +531,7 @@ def cluster_wise_analysis():
             schedule=overall_schedule
             sum_timings=list(itertools.chain.from_iterable(overall_sum_timings))
             #st.write(sum_timings)
-            if len(sum_timings)!=1: sum_timings=list(map(sum, zip(*sum_timings)))
+            if len(sum_timings)>1: sum_timings=list(map(sum, zip(*sum_timings)))
             #st.write(sum_timings)
             #st.write((shift_factor))
             cluster_shift=min(overall_schedule)
